9a968a1b4cd85bc9dc8e6a46c66511c4331d9e71
samandar-me <samandarcoderdev@gmail.com>
2024-02-17 13:34:35 +0500

    comment

--- composeApp/src/commonMain/kotlin/com/sdk/supergo/ui/human/HumanStoreFactory.kt
+++ composeApp/src/commonMain/kotlin/com/sdk/supergo/ui/human/HumanStoreFactory.kt
@@ -20,6 +20,11 @@ internal class HumanStoreFactory(
 
     private sealed interface Message {
         data object Loading: Message
+        data object OnFromChanged: Message
+        data object OnToChanged: Message
+        data class OnFromSelected(val city: String): Message
+        data class OnToSelected(val city: String): Message
+        data object OnReplaced:  Message
     }
     private inner class ExecutorImpl: CoroutineExecutor<HumanStore.Intent, Unit, HumanStore.State, Message, Nothing>(
         appDispatchers.main
@@ -29,12 +34,27 @@ internal class HumanStoreFactory(
         }
 
         override fun executeIntent(intent: HumanStore.Intent, getState: () -> HumanStore.State) {
-            
+            when(intent) {
+                is HumanStore.Intent.OnFromChanged -> dispatch(Message.OnFromChanged)
+                is HumanStore.Intent.OnToChanged -> dispatch(Message.OnToChanged)
+                is HumanStore.Intent.OnFromSelected -> dispatch(Message.OnFromSelected(intent.city))
+                is HumanStore.Intent.OnToSelected -> dispatch(Message.OnToSelected(intent.city))
+                is HumanStore.Intent.OnReplaced -> dispatch(Message.OnReplaced)
+            }
         }
     }
     private object ReducerImpl: Reducer<HumanStore.State, Message> {
         override fun HumanStore.State.reduce(msg: Message): HumanStore.State {
-            return copy()
+            return when(msg) {
+                is Message.Loading -> copy()
+                is Message.OnFromChanged -> copy(fromExpanded = !fromExpanded)
+                is Message.OnToChanged -> copy(toExpanded = !toExpanded)
+                is Message.OnReplaced -> {
+                    copy()
+                }
+                is Message.OnFromSelected-> copy(selectedCity1 = msg.city, fromExpanded = false)
+                is Message.OnToSelected-> copy(selectedCity2 = msg.city, toExpanded = false)
+            }
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/sdk/supergo/util/Log.kt
@@ -0,0 +1,10 @@
+package com.sdk.supergo.util
+
+import io.github.aakira.napier.Napier
+
+fun logE(msg: String) {
+    Napier.e(msg)
+}
+fun logI(msg: String) {
+    Napier.i(msg)
+}
